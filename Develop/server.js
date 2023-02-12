8052247ce2564ff541503fef633619fc11308ef2
tbgolfer86 <tbj86@outlook.com>
2023-02-12 15:39:29 -0500

    added GET routes for homepage and notes pages

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for feedback page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// // Wildcard route to direct users to a 404 page
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
