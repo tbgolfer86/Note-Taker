e493df9300d5ec05a90fc4604ba731b906088b65
tbgolfer86 <tbj86@outlook.com>
2023-02-12 20:18:08 -0500

    edited GET route to /api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const db = require('./db/db.json');
 const PORT = 3001;
 
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -22,11 +22,7 @@ app.get('*', (req, res) =>
 );
 
 // GET route to notes API
-app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) =>
-      res.json(JSON.parse(data))
-    );
-  });
+app.get('/api/notes', (req, res) => res.json(db));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
