439bad3a25382c1639d5d5609ce71b6f3d250a2a
tbgolfer86 <tbj86@outlook.com>
2023-02-12 21:04:07 -0500

    edited comments in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -11,15 +11,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET route to notes API
+// GET route for notes API
 app.get('/api/notes', (req, res) => res.json(db));
 
-// GET Route for feedback page
+// GET Route for homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
