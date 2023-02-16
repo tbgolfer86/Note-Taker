e3a27beb0b60975c5655f7f4411e5c175def4b68
tbgolfer86 <tbj86@outlook.com>
2023-02-15 20:16:41 -0500

    edited PORT in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
